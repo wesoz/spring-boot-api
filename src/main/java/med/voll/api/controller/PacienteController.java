25e5678941ee3d91daf5eb77607276e7a006d66d
Wesley de Paula <wesleyop@hotmail.com>
2023-12-17 19:57:47 -0300

    Implement paciente post and persistance

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println(dados);
+        this.repository.save(new Paciente(dados));
     }
+
 }
