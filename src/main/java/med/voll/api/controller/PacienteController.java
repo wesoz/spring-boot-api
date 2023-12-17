878e5eef9cfc456dc00398268ef1b1eff6104581
Wesley de Paula <wesleyop@hotmail.com>
2023-12-17 20:05:27 -0300

    Implement list and pagination for paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,13 +1,14 @@
 package med.voll.api.controller;
 
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -19,6 +20,11 @@ public class PacienteController {
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
         this.repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return this.repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 
 }
